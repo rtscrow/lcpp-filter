6058189c96165637df207f3ff9b72cd72633686d
rtscrow <mdds@duck.com>
2025-11-14 18:23:42 +0000

    remove comments based on static blocklist

--- lcppFilter.js
+++ lcppFilter.js
@@ -1,10 +1,25 @@
-blockedUsers = ['Alex']
-const allComments = document.querySelectorAll('.wpd-comment');
-for (const comment of allComments) {
-    // get username
-    username = comment.querySelector('.wpd-comment-wrap').querySelector('.wpd-comment-right').querySelector('.wpd-comment-author').querySelector('.wpd-comment-header').querySelector('.wpd-user-info').querySelector('.wpd-uinfo-top').querySelector('.wpd-comment-author').innerText;
-   // remove if in blocklist
-    if (username in blockedUsers) {
-        comment.style.display = 'none';
+blockedUsers = ['Alex', 'Jared', 'Winsor']
+
+function sleep(ms) { //https://stackoverflow.com/a/39914235
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+    while (document.readyState == 'complete') {
+        await sleep(50);
+        console.log("lccp-filter running")
+        const allComments = document.querySelectorAll('.wpd-comment'); 
+        for (const comment of allComments) {
+            // get username
+            username = comment.querySelector('.wpd-comment-author').innerText;
+            console.log("processing post from: " + username);
+            // remove if in blocklist
+            if (blockedUsers.includes(username)) {
+                console.log("blocking post from: " + username);
+                comment.style.display = 'none';
+            }
+        }
     }
 }
+
+main();
